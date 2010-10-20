53f03534c0e45514d046e327244c2ef15be50ab0
Mario G. Pozzo <camboris@gmail.com>
2010-10-19 23:56:18 -0300

    implementada la exportacion de imagen

--- OriCon.py
+++ OriCon.py
@@ -1,4 +1,4 @@
-import pygame,  sys,  os
+import pygame,  sys,  os,  Image
 from pygame.locals import *
 
 
@@ -6,6 +6,7 @@ class OriCon():
     SCREENRECT = Rect(0,0,800,600)
     imagen_activa = 0
     redibujar = False
+    imagen_pil = None
 
     def mostrarSiguiente(self):
         if self.imagen_activa + 1 >= len(self.lista_imagenes):
@@ -21,9 +22,38 @@ class OriCon():
             self.imagen_activa = len(self.lista_imagenes) - 1
         else:
             self.imagen_activa -= 1
-            
+        
         self.visor.mostrar("entrada/%s"%self.lista_imagenes[self.imagen_activa])
-
+    
+    def exportarImagen(self):
+        #calcular la imagen para exportar por pil
+        #si no esta, carga la imagen con pil
+        if not self.imagen_pil:
+            self.imagen_pil = Image.open("entrada/%s"%self.lista_imagenes[self.imagen_activa])
+        print self.imagen_pil
+        print self.visor.escala
+        print self.selector.posicion()[0]
+        x_real = self.selector.posicion()[0] - self.visor.offset_x
+        print x_real
+        ancho_real = self.selector.tamano()[0] / self.visor.escala
+        alto_real = self.selector.tamano()[1] / self.visor.escala
+        offset_x_real = x_real / self.visor.escala
+        offset_y_real = self.selector.posicion()[1] / self.visor.escala
+        print self.selector.tamano()[0]
+        print ancho_real
+        print self.selector.tamano()[1]
+        print alto_real
+        print offset_x_real
+        print offset_y_real
+        caja = (int(offset_x_real),  int(offset_y_real),  int(ancho_real + offset_x_real),  int(alto_real + offset_y_real))
+        print caja
+        recorte = self.imagen_pil.crop(caja)
+#        recorte = self.imagen_pil.crop((10, 10, 15, 15))
+        recorte = recorte.resize((600, 800))
+        recorte = recorte.convert("L")
+        recorte.save("salida/algo.jpg",  "JPEG")
+        
+    
     def __init__(self):
     #    creo la lista de imagenes
         self.lista_imagenes = os.listdir("entrada")
@@ -43,10 +73,10 @@ class OriCon():
         self.visor = Visor()
         self.visor.mostrar("entrada/%s"%self.lista_imagenes[self.imagen_activa])
         
-        selector = Selector()
+        self.selector = Selector()
 #        prueba = Prueba()
 #        sprites = pygame.sprite.RenderPlain((selector))
-        sprites = pygame.sprite.RenderUpdates((self.visor,  selector))
+        sprites = pygame.sprite.RenderUpdates((self.visor,  self.selector))
 #        self.ventana.blit(r,(0, 0))
         
         while True:
@@ -58,12 +88,13 @@ class OriCon():
                     if event.key is pygame.K_SPACE:
                         self.mostrarSiguiente()
                     elif event.key == pygame.K_LEFT:
-                        selector.achicar()
+                        self.selector.achicar()
                     elif event.key == pygame.K_RIGHT:
-                        selector.agrandar()
+                        self.selector.agrandar()
                     elif event.key is pygame.K_BACKSPACE:
                         self.mostrarAnterior()
-
+                    elif event.key is pygame.K_RETURN:
+                        self.exportarImagen()
             
             sprites.update()
             sprites.draw(self.ventana)
@@ -117,6 +148,8 @@ class Visor(pygame.sprite.Sprite):
             offset_x = (ventana_rect[2] - imagen_rect[2]) / 2
             offset_y = (ventana_rect[3] - imagen_rect[3]) / 2
             self.image.blit(imagen, (offset_x, offset_y))
+            self.escala = escala
+            self.offset_x = offset_x
         
 #        self.redibujar = True
         
@@ -138,6 +171,12 @@ class Selector(pygame.sprite.Sprite):
     def achicar(self):
         self.rect.inflate_ip(-6, -8)
         self.image = pygame.transform.scale(self.image,  self.rect.size)
+        
+    def posicion(self):
+        return self.rect.topleft
+        
+    def tamano(self):
+        return self.rect.size
         
 if __name__ == "__main__":
     OriCon()
